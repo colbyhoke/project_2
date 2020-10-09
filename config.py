a1bdc9e1a7ca9658b72ef4a126c1741807202c5d
Colby Hoke <choke@redhat.com>
2020-10-09 11:48:56 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+### config.py ###
+
+# Scheme: 'postgresql://<USERNAME>:<PASSWORD>@<HOST>:<PORT>/<DATABASE_NAME>'
+# Heroku login
+db_login_info = 'postgresql://vaetwnykonmsxk:8475b871d9028187109958cf2ba6d31067519d894622dcccf9d3c4401bb4059b@ec2-184-73-249-9.compute-1.amazonaws.com:5432/db9hptq0mroo2s'
+
+# Local login
+#db_login_info = 'postgresql://covid_db_admin:pass123@localhost:5432/covid_db'
